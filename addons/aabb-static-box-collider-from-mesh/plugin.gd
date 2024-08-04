e62efed66bea576183cf551739b3ecb20f3abe03
rybadour <rybadour@gmail.com>
2024-08-03 21:14:40 -0400

    Added some graveyard assets
     - Updated the aabb plugin to generate for a whole subtree of meshes.
     - Added a new zombie enemy
     - Added a graveyard test level

--- addons/aabb-static-box-collider-from-mesh/plugin.gd
+++ addons/aabb-static-box-collider-from-mesh/plugin.gd
@@ -1,13 +1,25 @@
 @tool
 extends EditorPlugin
 
-var currentNode: MeshInstance3D;
+var currentNode: Node3D;
 var generateButton: Button;
 
 func _handles(object) -> bool:
-	return object is MeshInstance3D;
+	if object is VisualInstance3D:
+		print_debug("Is Visual")
+		return true;
+	
+	elif object is Node3D:
+		var root: Node3D = object;
+		var children: Array = root.find_children("*", "VisualInstance3D");
+		print_debug("Has Visual?: " + str(!children.is_empty()));
+		
+		return !children.is_empty();
+	
+	return false;
 	
 func _edit(object: Object) -> void:
+	print_debug(object);
 	if object:
 		currentNode = object;
 	
@@ -28,7 +40,15 @@ func _make_visible(visible):
 	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, generateButton);
 	
 func _onButtonPressed():
-	var aabb = currentNode.mesh.get_aabb();
+	var aabb: AABB;
+	if currentNode is VisualInstance3D:
+		aabb = currentNode.mesh.get_aabb();
+	else:
+		var children: Array[Node] = currentNode.find_children("*", "VisualInstance3D");
+		var first: VisualInstance3D = children.pop_front();
+		aabb = getRelativeAABB(first);
+		for child in children:
+			aabb = aabb.merge(getRelativeAABB(child));
 	
 	var body = StaticBody3D.new();
 	body.name = "AABB_StaticBody3D";
@@ -46,3 +66,15 @@ func _onButtonPressed():
 	coll_shape.position = aabb.get_center();
 	body.add_child(coll_shape);
 	coll_shape.owner = currentNode.owner;
+
+
+func getRelativeAABB(childNode: Node3D):
+	var childAABB = childNode.get_aabb();
+	var parent = childNode.get_parent_node_3d();
+	while parent != currentNode:
+		var invTrans = parent.transform.inverse();
+		childAABB.size = invTrans.basis * childAABB.size;
+		childAABB.position -= invTrans.origin / invTrans.basis.get_scale();
+		print_debug("final: ", childAABB);
+		parent = parent.get_parent_node_3d();
+	return childAABB;
